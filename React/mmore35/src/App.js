cd0b00a84bd4f773b013b7b05cf7a1dd511314e5
Mike <mikemorel3322@gmail.com>
2022-03-14 19:37:18 -0500

    Start of react and tailwind implementation.
    "npm start" on mmore35 folder to start react project. Tailwind is broken for now.

---
+++ React/mmore35/src/App.js
@@ -0,0 +1,36 @@
+//import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <p className="text-green-400 text-left">
+//           hi mommy uh i mean
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-500">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App;
---
+++ React/mmore35/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
