9a89a8789c10f439a7fea572ea59d0a5e49fcf97
Mike <mikemorel3322@gmail.com>
2022-03-21 00:39:24 -0500

    working!
    all seems to be working now. Changes to layout icons and working email link

--- mmore35/src/FullPage.js
+++ mmore35/src/FullPage.js
@@ -3,23 +3,26 @@ import AboutPage from './AboutPage';
 import Projects from './Projects';
 
 const FullPage = () => {
-    return (
-        <body className='bg-BackGround-100 flex flex-row'>
-        <div className="basis-1/6 bg-stone-900">
-          <SideBar/>
-        </div>
-        <div className='basis-5/6'>
+  return (
+    <body className='bg-BackGround-100 flex flex-row'>
+      <div className="basis-1/6 bg-stone-900">
+        <SideBar />
+      </div>
+      <div className='basis-5/6'>
         <div className=''>
-        <AboutPage/>
+          <AboutPage />
         </div>
-        <div className='h-screen'>
-        <Projects/>
+        <div className=''>
+          <Projects />
         </div>
-        {/* TODO: make React the front page with a link to the html site */}
+        {/* TODO?: make React the front page with a link to the html site */}
         {/* TODO: games */}
-        </div>
-        </body>
-    );
+        <h1 className='MainText'>
+          This Site is still under construction! last update: March 21 2022 
+        </h1>
+      </div>
+    </body>
+  );
 };
 
 export default FullPage;
--- mmore35/src/SideBar.js
+++ mmore35/src/SideBar.js
@@ -1,22 +1,40 @@
-import { BsPlus, BsFillLightningFill } from 'react-icons/bs';
-import { FaFire, FaPoo } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+import { AiOutlineAppstore } from 'react-icons/ai';
+import { FaUserGraduate, FaGamepad } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   return (
     <div className="... p-1 h-screen fixed w-1/6">
-      <SideBarIcon icon={<FaFire className='' size="52" />} />
-      <SideBarIcon icon={<BsPlus size="32" />} />
-      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-      <SideBarIcon icon={<FaPoo size="20" />} />
-      {/* TODO: get real icons and set links with email*/}
+      <a>
+      <SideBarIcon icon={<FaUserGraduate size="52" />} text="About Me" />
+      </a>
+      <a>
+      <SideBarIcon icon={<AiOutlineAppstore size="60" />} text="My Projects"/>
+      </a>
+      <a>
+      <SideBarIcon icon={<FaGamepad size="60" />} text="My Games"/>
+      </a>
+      <a href='mailto:mmore35@lsu.edu'>
+      <SideBarIcon icon={<MdEmail size="60" />} text="Email Me" onClick ={mailMe}/>
+      </a>
+      {/* TODO: set links*/}
     </div>
   );
 };
 
-const SideBarIcon = ({ icon }) => (
+const SideBarIcon = ({ icon, text }) => (
   <div className="sidebar-icon group basis-1/6">
     {icon}
+    <span class="sidebar-tooltip group-hover:scale-100">{text}</span>
   </div>
 );
+
+const mailMe = ({ }) => {
+  return (
+    <a href={`mailto:mmore35@lsu.edu`}/>
+  );
+};
+
 
 export default SideBar;
