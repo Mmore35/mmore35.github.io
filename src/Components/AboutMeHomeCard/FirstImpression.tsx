8a692ef268b7338cf55c3248d6162d88340b26a8
Mike <57111855+Mmore35@users.noreply.github.com>
2024-08-13 21:55:07 -0500

    update goals

--- src/Components/AboutMeHomeCard/FirstImpression.tsx
+++ src/Components/AboutMeHomeCard/FirstImpression.tsx
@@ -2,13 +2,13 @@ export const FirstImpression = () => {
   return (
     <div>
       <div className="text-6xl text-indigo-100 font-bold pb-5 text-center">
-        はじめまして、
+        Hello, I'm
       </div>
       <div className="text-5xl text-indigo-100 font-bold pb-4 text-center">
         MICHAEL MOREL
       </div>
       <div className="text-indigo-100 text-2xl text-center">
-        Tokyo Based Full Stack Software Engineer
+        Full Stack Software Engineer
       </div>
     </div>
   );
--- src/Components/AboutMeHomeCard/SecondImpression.tsx
+++ src/Components/AboutMeHomeCard/SecondImpression.tsx
@@ -2,12 +2,12 @@ export const SecondImpression = () => {
   return (
     <div>
       <div className="text-indigo-100 text-xl mt-10 w-96">
-        <span className="font-bold text-4xl">My Objective</span> is to be
-        challenged creating state of the art software with others who share my
-        passion and drive for elegent solutions to complex systems.
+        <span className="font-bold text-4xl">My Goal</span> is to collaborate
+        with passionate individuals while elevating the standards expected of an
+        engineer.
       </div>
       <div className="text-indigo-100 text-xl mt-16 w-96">
-        <span className="font-bold text-4xl">My Goal</span> is to find like
+        <span className="font-bold text-4xl">My Objective</span> is to find like
         minded people to build a network of hard working career focused
         individuals spanning the world.
       </div>
